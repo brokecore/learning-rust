28362648425eef7fd877e79ba28e316aff06e277
brokecore <reversecel@tuta.com>
2025-12-17 14:04:46 -0600

    cargo

---
+++ cargo/target/debug/deps/cargo-181ce325d5814ecf.d
@@ -0,0 +1,5 @@
+/home/lain/Documents/GitHub/learning-rust/cargo/target/debug/deps/cargo-181ce325d5814ecf.d: src/main.rs
+
+/home/lain/Documents/GitHub/learning-rust/cargo/target/debug/deps/libcargo-181ce325d5814ecf.rmeta: src/main.rs
+
+src/main.rs:
