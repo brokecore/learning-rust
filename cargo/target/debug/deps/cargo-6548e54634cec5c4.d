5f7e6b49ba7dd724af0ce9f124f3eb1bd284b0d8
brokecore <reversecel@tuta.com>
2025-12-22 00:25:27 -0600

    cargo clean

--- cargo/target/debug/deps/cargo-6548e54634cec5c4.d
+++
@@ -1,5 +0,0 @@
-/home/lain/Documents/GitHub/learning-rust/cargo/target/debug/deps/cargo-6548e54634cec5c4.d: src/main.rs
-
-/home/lain/Documents/GitHub/learning-rust/cargo/target/debug/deps/libcargo-6548e54634cec5c4.rmeta: src/main.rs
-
-src/main.rs:
